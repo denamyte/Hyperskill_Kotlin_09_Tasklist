6cec4d8fad04c3829520bb8494a239dab98a690d
denys <denamyte@gmail.com>
2023-09-24 00:58:39 +0300

    2023-09-24: Stage 1/6

--- Tasklist/task/src/tasklist/Main.kt
+++ Tasklist/task/src/tasklist/Main.kt
@@ -1,7 +1,13 @@
 package tasklist
 
 fun main() {
-    // write your code here
+    println("Input the tasks (enter a blank line to end):")
+    generateSequence { readln().trim() }
+        .takeWhile(String::isNotEmpty)
+        .mapIndexed { i, s -> "%-3d$s".format(i + 1) }
+        .joinToString("\n")
+        .ifEmpty { "No tasks have been input" }
+        .let(::println)
 }
 
 

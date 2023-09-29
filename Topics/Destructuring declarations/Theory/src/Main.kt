53e53124404679997d4403fda93e349676ff8bcd
denys <denamyte@gmail.com>
2023-09-30 00:05:17 +0300

    2023-09-29: 1 problem

--- Topics/Destructuring declarations/Theory/src/Main.kt
+++ Topics/Destructuring declarations/Theory/src/Main.kt
@@ -1,5 +1,45 @@
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import java.io.File
+
 // You can experiment here, it won’t be checked
 
 fun main(args: Array<String>) {
-  // put your code here
+//    val baseDir = File("basedir")
+
+//    if (baseDir.isDirectory) {
+//        baseDir.walkBottomUp().maxByOrNull { f -> f.path.split(File.separator).size }
+//            .also { println(it!!.path) }
+//            .let { it!!.name }
+//            .let { println(it) }
+//    }
+
+//    if (baseDir.isDirectory) {
+//        baseDir.walkTopDown().filter { it.isDirectory && it.name != "basedir" }
+//            .maxByOrNull { it.listFiles()!!.size }
+//            .let { println("${it!!.name} ${it.listFiles()!!.size}") }
+//    }
+
+//    if (baseDir.isDirectory) {
+//        baseDir.walkTopDown()
+//            .filter { it.isDirectory && it.listFiles()!!.isEmpty() }
+//            .joinToString(" ") { it.name }
+//            .let(::println)
+//    }
+
+//    class Tourist(val name: String, val age: Int, val visitedCountries: Array<String>)
+//
+//    val moshi = Moshi.Builder()
+//        .add(KotlinJsonAdapterFactory())
+//        .build()
+//
+//    val touristAdapter = moshi.adapter(Tourist::class.java)
+//
+//    val touristString = """
+//        {"name":"Jacky",
+//        "age":23,
+//        "visitedCountries":["Israel","Argentina", "Malaysia"]}""".trimIndent()
+//
+//    val newTourist = touristAdapter.fromJson(touristString)
+//    newTourist?.visitedCountries.contentToString().let(::println)
 }
